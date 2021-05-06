fb9bd6f64bf6f6b802ff5e38348c0788c29c77f5
alexhla <astutealex@gmail.com>
2021-05-05 20:09:33 -0400

    added rvol scanner

--- robinhood.py
+++ robinhood.py
@@ -1,13 +1,15 @@
 from argparse import ArgumentParser
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from sys import stdout
+import datetime
+import time
 import pandas as pd
 
-# import robin_stocks as r  # package name differs with version
+#import robin_stocks as r  # package name differs with version
 import robin_stocks.robinhood as r
 
 # local imports
 import config
+import stockdictionary
 
 # Login to Robinhood
 r.login(config.ROBINHOOD_EMAIL, config.ROBINHOOD_PASSWORD)
@@ -19,46 +21,69 @@ ap = ArgumentParser()
 ap.add_argument('-i', '--info', nargs='+', metavar=['TICKERS'],
 	help='')
 
-ap.add_argument('-rvol', '--relative_volume_scanner', nargs=1, metavar=['INTERVAL'],
+
+ap.add_argument('-rvol', '--rvol_scanner', action='store_true',
+	help='')
+
+ap.add_argument('-v', '--volume_scanner', nargs=1, metavar=['INTERVAL'],
 	help='')
 
 # Parse user arguments
 args = vars(ap.parse_args())
 print(f'args --- {args}\n')
 
-if args['relative_volume_scanner']:
 
-	def animate(i):
-		fundementals = r.stocks.get_fundamentals(tickers)
-		df = pd.DataFrame.from_dict(fundementals)
-		df['rvol'] = df['volume'].astype(float) / df['average_volume'].astype(float)
-		rvol.append(df['rvol'].round(decimals=3).tolist())
-		ax1.clear()
-		ax1.plot(rvol)
-		ax1.set_xticks([])
-		ax1.legend(tickers, bbox_to_anchor=(1.05, 0.5), loc='center')
-		# print(f'ʕ•ᴥ•ʔ Tickers \t {tickers}\n')
-		# print(f'ʕ•ᴥ•ʔ RVOL \t {rvol}\n')
-		# print(dir(ax1))
 
-	interval = int(args['relative_volume_scanner'][0])
+if args['rvol_scanner']:
+
+	tickers = stockdictionary.tickers['microcap_float10M_price10USD']
+	data = {'ticker':tickers}
+	query_size = 100
+	query_list = []
 	rvol = []
-	tickers = ['PXS', 'NLSP', 'EVK', 'JOB', 'AEI', 
-			'SEAC', 'CREG', 'AHPI', 'WNW', 'MDRR',
-			'CASI', 'ASTC', 'WIMI', 'EVFM', 'MDLY',
-			'BTX',]
 
-	plt.style.use('seaborn-dark')
-	fig = plt.figure(figsize=(12, 8))
-	fig.canvas.set_window_title(f'RVOL Scanner  |  {interval} Second Interval')
-	ax1 = fig.add_subplot(1,1,1)
-	handle = animation.FuncAnimation(fig, animate, interval=(interval*1000))
-	plt.show()
+	# Build Query List
+	for i in range(0, len(tickers), query_size):
+		query_list.append(tickers[i:i+query_size])
+
+	# Get Volume
+	for q in query_list:
+		fundementals = r.stocks.get_fundamentals(q)
+		df = pd.DataFrame.from_dict(fundementals)
+		for col in df.columns:
+			if 'volume' in col:
+				data.setdefault(col, []).append(df[col].astype(float).tolist())
+	# # Compute RVOL
+	# for i in range(0, len(data['ticker'])):
+	# 	data.setdefault('RVOL', []).append(data['volume'][i] / data['average_volume'][i])
+	# 	data.setdefault('RVOL_2_weeks', []).append(data['volume'][i] / data['average_volume_2_weeks'][i])
+
+	# # Generate List of (Ticker, RVOL) Tuple Pairs for Sorting
+	# for i in range(0, len(data['ticker'])):
+	# 	rvol.append((data['ticker'][i], data['RVOL'][i]))
+	# rvol.sort(key=lambda x:x[1])
 
 
+	# for x in rvol:
+	# 	print(x)
 
 
+	print(f'ʕಠಿᴥಠʔ Tickers are {data["ticker"]} {type(data["ticker"])}\n')
+	# print(f'ʕಠಿᴥಠʔ Ticker Count is {len(data["ticker"])}\n')
+	# print(f'ʕಠಿᴥಠ ʔtytRequest is {request} {type(request)}\n')
+	# print(f'ʕಠಿᴥಠʔ Fundementals are {fundementals} {type(fundementals)}\n')
+	# print(f'ʕಠಿᴥಠʔ Data is {data} {type(data)}\n')
+	print(f'ʕಠಿᴥಠʔ Volume is {data["volume"]} {type(data["volume"])}\n')
+	# print(f'ʕಠಿᴥಠʔ Volume Count is {len(data["volume"])}\n')
+	# print(f'ʕಠಿᴥಠʔ Average Volume is {data["average_volume"]} {type(data["average_volume"])}\n')
+	# print(f'ʕಠಿᴥಠʔ Average Volume Count is {len(data["average_volume"])}\n')
+	# print(f'ʕಠಿᴥಠʔ RVOL is {data["RVOL"]} {type(data["RVOL"])}\n')
 
+	# for key in data:
+	# 	print(f'{key} {len(data[key])}')
+
+	# Wait
+	# time.sleep(60)
 
 
 
@@ -72,14 +97,6 @@ if args['info']:
 	instrument = r.stocks.find_instrument_data(tickers)
 	data['stock_exchange'] = instrument[0]['market'].split('/')[-2]
 
-	# Convert MIC Code to Exchange Name
-	if data['stock_exchange'] == 'XNYS':
-		data['stock_exchange'] = ' '.join((data['stock_exchange'], '(NYSE)'))
-	if data['stock_exchange'] == 'XNAS':
-		data['stock_exchange'] = ' '.join((data['stock_exchange'], '(NASDAQ)'))
-	if data['stock_exchange'] == 'XASE':
-		data['stock_exchange'] = ' '.join((data['stock_exchange'], '(AMEX)'))
-
 	# Get Lastest Trade Price
 	latest_price = r.stocks.get_latest_price(tickers, priceType=None, includeExtendedHours=True)
 	data['latest_price'] = round(float(latest_price[0]), 2)
@@ -91,9 +108,14 @@ if args['info']:
 		if any(word in col for word in ['market_cap', 'volume', 'shares', 'float']):
 			data[col] = round(float(df[col][0]), 2)
 
-	# Compute Data
+	# Compute RVOL
 	data['RVOL'] = round(data['volume'] / data['average_volume'], 2)
 	data['RVOL_2_weeks'] = round(data['volume'] / data['average_volume_2_weeks'], 2)
+
+	# Get Volume History
+	# historicals = r.stocks.get_stock_historicals(tickers, interval='5minute', span='week')#, bounds='extended')
+	# for d in historicals:
+	# 	data.setdefault('volume_history', []).append((d['begins_at'], d['volume']))
 
 	# Print Data 
 	for k,v in data.items():
---
+++ stockdictionary.py
@@ -0,0 +1,85 @@
+tickers = {}
+
+tickers['microcap_float10M_price10USD'] = [
+
+'AAME', 'ABIO', 'ACOR', 'ACY', 'ADXN', 'AEHL', 'AEI', 'AEY',
+'AGFY', 'AHPI', 'AIH', 'AINC', 'AMBO', 'AMPG', 'AMS', 'AMST',
+'ANPC', 'ANY', 'APDN', 'APM', 'APOP', 'APWC', 'ARDS', 'ARTW',
+'ASPS', 'ATXI', 'AUTO', 'AUUD', 'AUVI', 'AVCT', 'AWX', 'AXAS',
+'AZYO',
+
+'BBGI', 'BBIG', 'BCAC', 'BCDA', 'BCTX', 'BDR', 'BGI', 'BIMI',
+'BITE', 'BKYI', 'BLCM', 'BLIN', 'BLPH', 'BMRA', 'BMTX', 'BNSO',
+'BNTC', 'BOAS', 'BOSC', 'BPTH', 'BRN', 'BSBK', 'BYFC',
+
+'CAHC', 'CARV', 'CBLI', 'CDOR', 'CELP', 'CHCI', 'CHFW', 'CHNR',
+'CLEU', 'CLPS', 'CLWT', 'CNEY', 'CNFR', 'CNTG', 'CODA', 'CPIX',
+'CPSH', 'CREG', 'CREX', 'CRKN', 'CRTD', 'CRWS', 'CSPI', 'CTEK',
+'CTIB', 'CUEN', 'CVV', 'CXDO', 'CYAN', 'CYCC', 'CYTH',
+
+'DAIO', 'DLCA', 'DLPN', 'DSWL', 'DTSS', 'DUOT', 'DXF', 'DXR',
+
+'EAST', 'EDSA', 'EDTK', 'EFOI', 'ELA', 'ELMD', 'ELSE', 'ELTK',
+'ELYS', 'ENSV', 'EQS', 'EVAX', 'EVK', 'EVOL', 'EYEG', 'EZGO',
+
+'FAMI', 'FFHL', 'FGF', 'FNHC', 'FORD', 'FSI', 'FSSI', 'FSTX',
+'FWP',
+
+'GBNY', 'GBR', 'GBS', 'GIGM', 'GLBS', 'GNRS', 'GOED', 'GOVX',
+'GTEC', 'GTIM', 'GURE', 'GWGH',
+
+'HIHO', 'HJLI', 'HOTH', 'HTGM', 'HUDI', 'HUSA', 'HUSN', 'HX',
+
+'ICD', 'ID', 'IFMK', 'IHT', 'IKNX', 'IKT', 'IMAC', 'IMRN',
+'IMTE', 'INDO', 'INM', 'INVO', 'IPDN', 'IPWR', 'ISIG', 'ISNS',
+'ISUN',
+
+'JAKK', 'JAN', 'JCS', 'JCTCF', 'JILL', 'JMP', 'JP', 'JRSH',
+'JUPW', 'JVA',
+
+'KBNT', 'KBSF', 'KFFB', 'KINS',
+
+'LEDS', 'LEGO', 'LEXX', 'LGVN', 'LIXT', 'LMNL', 'LNSR', 'LOAN',
+'LTBR', 'LWAY', 'LXEH', 'LYL', 'LYRA',
+
+'MARPS', 'MBOT', 'MDIA', 'MDJH', 'MDLY', 'MDRR', 'MEDS', 'MFH',
+'MITC', 'MITO', 'MKTY', 'MNPR', 'MOSY', 'MOXC', 'MRIN', 'MSN',
+'MTBC', 'MTC', 'MTP', 'MTR', 'MTSL', 'MVO', 'MXC', 'MYMD',
+'MYO', 'MYT',
+
+'NEON', 'NEPH', 'NES', 'NETE', 'NEWA', 'NHIC', 'NHTC', 'NNVC',
+'NRT', 'NSPR', 'NSYS', 'NTEC', 'NTWK', 'NURO', 'NUWE', 'NUZE',
+'NVFY', 'NVOS',
+
+'OBLN', 'OCC', 'OCUP', 'OLB', 'OMEX', 'ONVO', 'OPHC', 'OSN',
+'OXBR', 
+
+'PALI', 'PBLA', 'PBTS', 'PCSA', 'PFIN', 'PHCF', 'PIXY', 'PLAG',
+'PMD', 'PNBK', 'PPIH', 'PPSI', 'PRT', 'PSHG', 'PSTV', 'PTPI',
+'PXS',
+
+'QH', 'QLI', 
+
+'RCMT', 'RCON', 'RELI', 'RFIL', 'RMCF', 'RMED', 'RNGR', 'RVPH',
+'RZLT',
+
+'SCKT', 'SCPS', 'SCX', 'SELF', 'SGBX', 'SGLB', 'SGMA', 'SGRP',
+'SIEB', 'SJ', 'SLGL', 'SMIT', 'SNES', 'SNFCA', 'SNOA', 'SQFT',
+'SSNT', 'SSY', 'STRR', 'SVT', 'SYNL', 'SYTA',
+
+'TAIT', 'TAOP', 'TATT', 'TC', 'TESS', 'THMO', 'TRMT', 'TRT',
+'TSRI', 'TTNP', 'TURN',
+
+'UFAB', 'UGRO', 'USAP', 'USDP', 'USEG', 'UUU',
+
+
+'VINO', 'VIRI', 'VIVE', 'VLON', 'VMAR', 'VRME', 'VRPX', 'VTSI',
+'VVPR', 'VYNT',
+
+'WAFU', 'WHLM', 'WHLR', 'WINT', 'WISA', 'WKEY', 'WORX', 'WYY',
+
+'XBIO', 'XELB', 'XTLB', 'XYF',
+
+'YGMZ', 'YTEN', 'YVR',
+
+]
