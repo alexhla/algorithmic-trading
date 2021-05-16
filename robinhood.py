21e75f0087b521374070ba1043b95793cce3ecfe
alexhla <astutealex@gmail.com>
2021-05-16 17:11:09 -0400

    expanded rvol (sizzle) scanner to 13m

--- robinhood.py
+++ robinhood.py
@@ -1,10 +1,7 @@
 from argparse import ArgumentParser
-from datetime import datetime
 from pandas import DataFrame
 from time import sleep
 from sys import stdout
-from csv import writer
-from subprocess import run
 
 #import robin_stocks as r  # package name differs with version
 import robin_stocks.robinhood as r
@@ -22,7 +19,7 @@ ap = ArgumentParser()
 # Add the arguments to the parser
 ap.add_argument('-i', '--info', nargs='+', metavar=['TICKERS'])
 ap.add_argument('-vc', '--volume_checker', action='store_true')
-ap.add_argument('-vs', '--volume_scanner', action='store_true')
+ap.add_argument('-ss', '--sizzle_scanner', action='store_true')
 
 # Parse user arguments
 args = vars(ap.parse_args())
@@ -93,45 +90,6 @@ if args['volume_checker']:
 	print(df_filtered['symbol'].tolist())
 
 
-'''
-
-Get Volume History Maximum
-
-'''
-def get_volume_history_maximums(selection):
-
-	tickers = watchlists.tickers[selection]
-	volume_history = {}
-	query_size = 75
-	query_list = []
-	for i in range(0, len(tickers), query_size):
-		query_list.append(tickers[i:i+query_size])
-
-	# Get Volume History
-	historicals_5min_week = []
-	historicals_hour_month = []
-	for i, q in enumerate(query_list):
-		print(f'Requesting Volume History Query List {i+1} of {len(query_list)}')
-		
-		history = r.stocks.get_stock_historicals(q, interval='5minute', span='week')
-		for h in history:
-			historicals_5min_week.append({key: h[key] for key in h.keys() & {'begins_at', 'symbol', 'volume', 'session'}})
-			
-		history = r.stocks.get_stock_historicals(q, interval='hour', span='month')
-		for h in history:
-			historicals_hour_month.append({key: h[key] for key in h.keys() & {'begins_at', 'symbol', 'volume', 'session'}})
-		
-	df_5min_week = DataFrame.from_dict(historicals_5min_week)	
-	df_hour_month = DataFrame.from_dict(historicals_hour_month)
-
-	# Get Max Volume for Each Timeframe
-	for t in tickers:
-		max_5min_week = df_5min_week[df_5min_week["symbol"] == t].max().to_dict()['volume']
-		max_hour_month = df_hour_month[df_hour_month["symbol"] == t].max().to_dict()['volume']
-		volume_history[t] = [max_5min_week, max_hour_month]
-
-	return volume_history
-
 
 '''
 
@@ -139,9 +97,9 @@ Scanner
 
 '''
 
-if args['volume_scanner']:
+if args['sizzle_scanner']:
 
-	tickers = watchlists.tickers['foobar']
+	tickers = watchlists.tickers['schwab_05_2020']
 	rvol = {}
 	rvol['history'] = []
 	query_size = 100  # 100 max per API Request
@@ -150,6 +108,7 @@ if args['volume_scanner']:
 		query_list.append(tickers[i:i+query_size])
 
 	while True:
+		volume = []
 		rvol['current'] = []
 		rvol['30s'] = []
 		rvol['60s'] = []
@@ -158,19 +117,20 @@ if args['volume_scanner']:
 		rvol['3m'] = []
 		rvol['4m'] = []
 		rvol['5m'] = []
-		rvol['10m'] = []
-		rvol['15m'] = []
+		rvol['7m'] = []
+		rvol['9m'] = []
+		rvol['11m'] = []
+		rvol['13m'] = []
 
 		# Get Current Volume
-		temp_volume = []
 		for q in query_list:
 			fundementals = r.stocks.get_fundamentals(q)
 			for f in fundementals:
-				temp_volume.append({key: f[key] for key in f.keys() & {'volume', 'average_volume'}})
+				volume.append({key: f[key] for key in f.keys() & {'volume', 'average_volume'}})
 
 		# Convert to Data Frame and Compute RVOL
-		df = DataFrame.from_dict(temp_volume)
-		df['RVOL'] = (df['volume'].astype(float) / df['average_volume'].astype(float)).round(3)
+		df = DataFrame.from_dict(volume)
+		df['RVOL'] = (df['volume'].astype(float) / df['average_volume'].astype(float)).round(2)
 		rvol['history'].append(df['RVOL'].tolist())
 
 		# Create (Ticker, RVOL) Pairs for Sorting Current RVOL
@@ -193,10 +153,14 @@ if args['volume_scanner']:
 				rvol['4m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-9][i]))
 			if len(rvol['history']) >= 11:
 				rvol['5m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-11][i]))
-			if len(rvol['history']) >= 21:
-				rvol['10m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-21][i]))
-			if len(rvol['history']) >= 31:
-				rvol['15m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-31][i]))
+			if len(rvol['history']) >= 15:
+				rvol['7m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-15][i]))
+			if len(rvol['history']) >= 19:
+				rvol['9m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-19][i]))
+			if len(rvol['history']) >= 23:
+				rvol['11m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-23][i]))
+			if len(rvol['history']) >= 27:
+				rvol['13m'].append((tickers[i], rvol['history'][-1][i] - rvol['history'][-27][i]))
 
 		# Sort
 		rvol['current'].sort(key=lambda x:x[1])
@@ -204,24 +168,54 @@ if args['volume_scanner']:
 		rvol['60s'].sort(key=lambda x:x[1])
 		rvol['90s'].sort(key=lambda x:x[1])
 		rvol['2m'].sort(key=lambda x:x[1])
-		rvol['4m'].sort(key=lambda x:x[1])
 		rvol['3m'].sort(key=lambda x:x[1])
+		rvol['4m'].sort(key=lambda x:x[1])
 		rvol['5m'].sort(key=lambda x:x[1])
-		rvol['10m'].sort(key=lambda x:x[1])
-		rvol['15m'].sort(key=lambda x:x[1])
+		rvol['7m'].sort(key=lambda x:x[1])
+		rvol['9m'].sort(key=lambda x:x[1])
+		rvol['11m'].sort(key=lambda x:x[1])
+		rvol['13m'].sort(key=lambda x:x[1])
 
-		if len(rvol['history']) == 2:
+		# Display Results
+		if len(rvol['history']) >= 27:  # 13m
 			for i in range(len(tickers)):
-				print(f'{rvol["current"][i][0]:<6}{rvol["current"][i][1]:<10}{rvol["30s"][i][0]:<6}{rvol["30s"][i][1]:<10}')
-			print('RVOL{:<12}30s Δ'.format(' '))
-		# elif len(rvol) == 3:
-		# 	for i in range(0, len(tickers)):
-		# 		print(f'{rvol_total[i][0]:<6}{rvol_total[i][1]:<10}{rvol_30s[i][0]:<6}{rvol_30s[i][1]:<10}{rvol_60s[i][0]:<6}{rvol_60s[i][1]:<10}')
-		# 	print('RVOL{:<12}30s{:<6}60s'.format(' ', ' '))
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}{rvol["5m"][i][0]:<5}{rvol["5m"][i][1]:<7}{rvol["7m"][i][0]:<5}{rvol["7m"][i][1]:<7}{rvol["9m"][i][0]:<5}{rvol["9m"][i][1]:<7}{rvol["11m"][i][0]:<5}{rvol["11m"][i][1]:<7}{rvol["13m"][i][0]:<5}{rvol["13m"][i][1]:<7}')
+		elif len(rvol['history']) >= 23 and len(rvol['history']) <= 26:  # 11m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}{rvol["5m"][i][0]:<5}{rvol["5m"][i][1]:<7}{rvol["7m"][i][0]:<5}{rvol["7m"][i][1]:<7}{rvol["9m"][i][0]:<5}{rvol["9m"][i][1]:<7}{rvol["11m"][i][0]:<5}{rvol["11m"][i][1]:<7}')
+		elif len(rvol['history']) >= 19 and len(rvol['history']) <= 22:  # 9m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}{rvol["5m"][i][0]:<5}{rvol["5m"][i][1]:<7}{rvol["7m"][i][0]:<5}{rvol["7m"][i][1]:<7}{rvol["9m"][i][0]:<5}{rvol["9m"][i][1]:<7}')
+		elif len(rvol['history']) >= 15 and len(rvol['history']) <= 18:  # 7m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}{rvol["5m"][i][0]:<5}{rvol["5m"][i][1]:<7}{rvol["7m"][i][0]:<5}{rvol["7m"][i][1]:<7}')
+		elif len(rvol['history']) >= 11 and len(rvol['history']) <= 14:  # 5m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}{rvol["5m"][i][0]:<5}{rvol["5m"][i][1]:<7}')
+		elif len(rvol['history']) == 9 or len(rvol['history']) == 10:  # 4m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}{rvol["4m"][i][0]:<5}{rvol["4m"][i][1]:<7}')
+		elif len(rvol['history']) == 7 or len(rvol['history']) == 8:  # 3m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}{rvol["3m"][i][0]:<5}{rvol["3m"][i][1]:<7}')
+		elif len(rvol['history']) == 5 or len(rvol['history']) == 6:  # 2m
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}{rvol["2m"][i][0]:<5}{rvol["2m"][i][1]:<7}')
+		elif len(rvol['history']) == 4:  # 90s
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}{rvol["90s"][i][0]:<5}{rvol["90s"][i][1]:<7}')
+		elif len(rvol['history']) == 3:  # 60s
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]:<7}{rvol["60s"][i][0]:<5}{rvol["60s"][i][1]:<7}')
+		elif len(rvol['history']) == 2:  # 30s
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<7}{rvol["30s"][i][0]:<5}{rvol["30s"][i][1]}')
+		else: # elif len(rvol['history']) == 1:
+			for i in range(len(tickers)):
+				print(f'{rvol["current"][i][0]:<5}{rvol["current"][i][1]:<6}')
 
-			# for i in range(0, len(tickers)):
-			# 	print(f'{rvol_total[i][0]:<6}{rvol_total[i][1]:<10}{rvol_30s[i][0]:<6}{rvol_30s[i][1]:<10}{rvol_60s[i][0]:<6}{rvol_60s[i][1]:<10}{rvol_90s[i][0]:<6}{rvol_90s[i][1]:<12}')
-			# print('RVOL{:<12}30s{:<6}60s{:<8}90s'.format(' ', ' ', ' '))
+		print('RVOL{:<8}30s Δ{:<7}60s Δ{:<7}90s Δ{:<7}2m Δ{:<8}3m Δ{:<8}4m Δ{:<8}5m Δ{:<8}7m Δ{:<8}9m Δ{:<8}11m Δ{:<7}13m Δ{:<7}'.format('','','','','','','','','','','',''))
+
 
 		# Debug 
 		# print(f'ʕಠಿᴥಠʔ Tickers are {tickers} --- Type: {type(tickers)} --- Length: {len(tickers)}\n')
@@ -230,13 +224,13 @@ if args['volume_scanner']:
 		# print(f'ʕಠಿᴥಠʔ RVOL Current is {rvol_total} --- Type: {type(rvol_total)} --- Length: {len(rvol_total)}\n')
 		# print(f'ʕಠಿᴥಠʔ RVOL 30s is {rvol_30s} --- Type: {type(rvol_30s)} --- Length: {len(rvol_30s)}\n')
 		# print(f'ʕಠಿᴥಠʔ RVOL 60s is {rvol_60s} --- Type: {type(rvol_60s)} --- Length: {len(rvol_60s)}\n')
-		# print(f'ʕಠಿᴥಠʔ RVOL 90s is {rvol_90s} --- Type: {type(rvol_90s)} --- Length: {len(rvol_90s)}\n')
 
 		# Wait
-		wait_seconds = 5
+		wait_seconds = 30
+		elapsed_minutes = ((len(rvol['history'])-1) * wait_seconds) / 60 
 		for i in range(0, wait_seconds+1):
 			stdout.write('\r')
-			stdout.write(f'Updating in {wait_seconds-i} Seconds...')
+			stdout.write(f'{elapsed_minutes} Minutes Elapsed. Updating in {wait_seconds-i} Seconds...')
 			stdout.flush()
 			sleep(1)
 		print('\n\nWorking...\n')
